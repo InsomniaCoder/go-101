e686dc731200de950bda12662da16977cebec28e
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2020-10-24 10:30:55 +0700

    add content for variables

---
+++ tutorials/variable-101.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+)
+
+func main() {
+
+	//Explicit
+	var anInteger int = 42
+	//default value = 0
+
+	var aString string = "Hello"
+
+	//Implicit use :=
+	implicitInteger := 42
+	implicitString := "Hello"
+
+	//constant = unchanging value
+	const integers int = 42
+	const anString = "Hello"
+
+}
